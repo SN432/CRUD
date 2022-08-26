4855f754a86098115343b7912e102682bb7237ea
Your Name <you@example.com>
2022-08-26 16:23:34 +0700

    added

---
+++ crud-project/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Crud from '../src/component/Crud'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Crud/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
